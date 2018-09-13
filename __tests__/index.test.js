8c68a267c4d563a1b0ce1463d7ecca2f1032931c
Cythilya Tang <cythilya@gmail.com>
2018-09-13 23:12:35 +0800

    index test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,88 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import App from '../pages/index';
 
-import Header from '../components/header';
+describe('Test page index is working or not...', () => {
+  const initialState = {
+    nearbyStoresData: [{
+      id: '12345',
+      name: 'ABCDE',
+      image: [
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+      ],
+      tags: {
+        ABC: true,
+      },
+      booking: {
+        phone: '02-12345678',
+      },
+      location: {
+        address: 'ABCDEF',
+      },
+    }],
+    recommendStoresData: [{
+      id: '12345',
+      name: 'ABCDE',
+      image: [
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+      ],
+      tags: {
+        ABC: true,
+      },
+      booking: {
+        phone: '02-12345678',
+      },
+      location: {
+        address: 'ABCDEF',
+      },
+    }],
+    hotStoresData: [{
+      id: '12345',
+      name: 'ABCDE',
+      image: [
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+        { url: 'https://dummyimage.com/600x400/000/fff' },
+      ],
+      tags: {
+        ABC: true,
+      },
+      booking: {
+        phone: '02-12345678',
+      },
+      location: {
+        address: 'ABCDEF',
+      },
+    }],
+  };
+  const mockStore = configureStore();
+  const store = mockStore(initialState);
+  const container = shallow(<App store={store} />);
 
-describe('With Enzyme', () => {
-  it('title: 吃什麼，どっち', () => {
-    const app = shallow(<Header />);
-    expect(app.find('.header__title').text()).toEqual('吃什麼，どっち');
+  it('Test elements in header component do exist', () => {
+    expect(container.length).toEqual(1);
   });
 });
 
