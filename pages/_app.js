851216404cc736bf39834ddf4bf65153144c2fb2
summer_tang <cythilya@gmail.com>
2020-08-14 11:18:58 +0800

    fix chakra UI problem by upgrading react and react-dom to version 16.8.6, ref: https://stackoverflow.com/questions/54101839/react-default-usecontext-is-not-a-function/54102205

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import withReduxStore from '../lib/with-redux-store';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
-      <Container>
-        <Provider store={reduxStore}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <ThemeProvider>
+        <CSSReset />
+        <Container>
+          <Provider store={reduxStore}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+      </ThemeProvider>
     );
   }
 }
